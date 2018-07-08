c73a974d135cfc2e47d0ed851ec83bbf56f5427a
Shawn-Gallea123 <shawngallea@gmail.com>
2018-07-08 17:24:55 -0400

    Changed to public IP

--- app/src/main/java/shawn/com/chatapp/ServerInterface.java
+++ app/src/main/java/shawn/com/chatapp/ServerInterface.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 
 // Variables for interfacing with server
 public class ServerInterface {
-    public static String serverIP = "192.168.1.192"; // Server IP address
+    public static String serverIP = "70.27.120.214"; // Server IP address
     public static boolean firstTime = true; // First time connecting flag
     public static BufferedReader fromServer = null; // Input from server
     public static int port = 60000; // Server listening port
